b6b8c380a0a716e427f4934cc8746c6cc6e7faa8
rcspell <136676662+ChhaviRohilla@users.noreply.github.com>
2026-06-26 18:07:35 +0530

    Update CharRepeat.java

--- CharRepeat.java
+++ CharRepeat.java
@@ -2,39 +2,31 @@ import java.util.*;
 
 public class CharRepeat {
 
-    public static void main(String args[]) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String str = sc.next();
-
-        StringBuilder result = new StringBuilder();
-
-        int count = 1;
-
-
-        for(int i = 0; i < str.length()-1; i++) {
-
-            if(str.charAt(i) == str.charAt(i+1)) {
-
-                count++;
-
-            }
-            else {
-
-                result.append(str.charAt(i));
-                result.append(count);
-
-                count = 1;
-            }
+    public static String encode(String text) {
+        if (text == null || text.isEmpty()) {
+            return "";
         }
 
+        StringBuilder encoded = new StringBuilder();
+        int count = 1;
 
-        // add last character
-        result.append(str.charAt(str.length()-1));
-        result.append(count);
+        for (int i = 0; i < text.length(); i++) {
+            // If the next character is the same, increment our tracking counter
+            if (i + 1 < text.length() && text.charAt(i) == text.charAt(i + 1)) {
+                count++;
+            } else {
+                // Append the character followed by its total count
+                encoded.append(text.charAt(i)).append(count);
+                count = 1; // Reset counter for the next character run
+            }
+        }
+        return encoded.toString();
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
 
-
-        System.out.println(result);
+        String res = encode(str);
+        System.out.println(res);
     }
 }
